a4db2b776bd249c1929e0aa4bb4e1c781e22ce43
Ava Gordon <avagordon01@gmail.com>
2025-02-06 09:29:52 +0000

    cmake ninja multi-config

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 set -eux
 
 if [ ! -d build ]; then
-    mkdir -p build/{debug,release,tsan}
-    cmake -G Ninja -S . -B build/debug -DCMAKE_BUILD_TYPE=Debug || rm -rf build
-    cmake -G Ninja -S . -B build/tsan -DCMAKE_BUILD_TYPE=TSan || rm -rf build
-    cmake -G Ninja -S . -B build/release -DCMAKE_BUILD_TYPE=Release || rm -rf build
+    mkdir -p build
+    cmake -G "Ninja Multi-Config" -S . -B build || rm -rf build
 fi
 if [ -d build ]; then
-    cmake --build build/debug
-    cmake --build build/tsan
-    cmake --build build/release
-    cmake --install build/release --prefix install
+    config=Debug
+    #config=TSan
+    #config=Release
+    cmake --build build --config ${config}
+    cmake --test build --config ${config}
+    cmake --install build --config ${config} --prefix install
 fi
--- test.sh
+++ test.sh
@@ -4,8 +4,4 @@ set -eux
 
 ./build.sh
 
-ctest --test-dir ./build/debug
-ctest --test-dir ./build/tsan
-ctest --test-dir ./build/release
-
-./build/release/starter
+./install/bin/starter
